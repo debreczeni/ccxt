05127aa0acb0096fba3fca156e143178424c742b
David Debreczeni <david.debreczeni@gmail.com>
2019-05-13 04:15:47 +0200

    WebMock WIP

--- ruby/spec/lib/ccxt/base/exchange_spec.rb
+++ ruby/spec/lib/ccxt/base/exchange_spec.rb
@@ -8,7 +8,29 @@ RSpec.describe Ccxt::Exchange do
     let(:exchange) { Ccxt::Kraken.new exchange_config }
     let(:symbol) { 'BTC/EUR' }
 
-    it 'testSymbol' do
+    let(:stub_with) do
+      {
+        headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip, deflate',
+          'Host'=>'api.kraken.com',
+          'User-Agent'=>'rest-client/2.0.2 (darwin16.7.0 x86_64) ruby/2.3.4p301'
+        }
+      }
+    end
+
+    before do
+      stub_request(:get, "https://api.kraken.com/0/public/AssetPairs").
+        with(stub_with).to_return(status: 200, body: "", headers: {})
+      stub_request(:get, "https://api.kraken.com/0/public/Assets").
+        with(stub_with).to_return(status: 200, body: "", headers: {})
+      stub_request(:get, "https://support.kraken.com/hc/en-us/articles/205893708-What-is-the-minimum-order-size-").
+        with(stub_with).to_return(status: 200, body: "", headers: {})
+
+
+    end
+
+    it 'public endpoints' do
       exchange.fetch_markets
       exchange.fetch_currencies
       exchange.fetch_ticker symbol
@@ -19,15 +41,17 @@ RSpec.describe Ccxt::Exchange do
       exchange.fetch_l2_order_book symbol
     end
 
-    it 'loadExchange' do
-      exchange.load_markets
-      expect(exchange.markets).to be_a Hash
-      expect(exchange.symbols).to be_a Array
-      expect(exchange.symbols.length).to be > 1
-      expect(exchange.symbols.length).to eq exchange.markets.length
+    context '#load_markets' do
+      it 'loads market data into @markets and @symbols' do
+        exchange.load_markets
+        expect(exchange.markets).to be_a Hash
+        expect(exchange.symbols).to be_a Array
+        expect(exchange.symbols.length).to be > 1
+        expect(exchange.symbols.length).to eq exchange.markets.length
+      end
     end
 
-    it 'testExchange' do
+    it 'private endpoints' do
       next unless test_private
 
       exchange.fetch_balance
